9ac770aadf78f8ce9b836286b8aae800e3a8ae5a
ferrriii <p>
2019-11-17 15:40:48 +0330

    Clean iuid test

--- test/iuid.test.js
+++ test/iuid.test.js
@@ -2,21 +2,19 @@ import 'babel-polyfill'
 import iuid from '../src/modules/iuid.js'
 
 it('should generate unique Id', async () => {
-  let ids = new Set()
+  const ids = new Set()
   for (let i = 0; i < 100000; i++) {
     ids.add(iuid())
   }
   expect(ids.size).toBe(100000)
-
 })
 
 it('should generate sequential Id', async () => {
   let lastId = 0
   let newId = 0
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 20000; i++) {
     newId = iuid()
     expect(newId).toBeGreaterThan(lastId)
     lastId = newId
   }
-
 })
